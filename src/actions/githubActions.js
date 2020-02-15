85f8cc30ebcebca5f62bee9f57beea14a67ad195
Travis Molter <travismolter@gmail.com>
2020-02-14 16:24:14 -0800

    both actions created and tested, and services mocked

--- src/actions/githubActions.js
+++ src/actions/githubActions.js
@@ -1,4 +1,4 @@
-import { getGithubUser } from '../services/githubApi';
+import { getGithubUser, getRepos } from '../services/githubApi';
 
 export const FETCH_USERINFO_LOADING = 'FETCH_USERINFO_LOADING';
 export const FETCH_USERINFO = 'FETCH_USERINFO';
@@ -8,5 +8,16 @@ export const fetchUserInfo = loginName => dispatch => {
     .then(user => dispatch({
       type: FETCH_USERINFO,
       payload: user
+    }));
+};
+
+export const FETCH_REPO_LOADING = 'FETCH_REPO_LOADING';
+export const FETCH_REPO = 'FETCH_REPO';
+export const fetchRepo = loginName => dispatch => {
+  dispatch({ type: FETCH_REPO_LOADING });
+  return getRepos(loginName)
+    .then(repos => dispatch({
+      type: FETCH_REPO,
+      payload: repos
     }));
 };
--- src/services/githubApi.js
+++ src/services/githubApi.js
@@ -1,5 +1,5 @@
-export const getGithubUser = username => {
-  return fetch(` https://api.github.com/users/${username}`)
+const request = path => {
+  return fetch(`https://api.github.com/users${path}`)
     .then(res => Promise.all([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw json;
@@ -7,3 +7,6 @@ export const getGithubUser = username => {
       return json;
     });
 };
+
+export const getGithubUser = username => request(`/${username}`);
+export const getRepos = username => request(`/${username}/repos`);
